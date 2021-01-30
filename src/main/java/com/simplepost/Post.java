7ff32b7ebf130c4024419cbc99f382a8a530bf5c
GreenBudgie <forestengine@yandex.ru>
2021-01-30 21:05:53 +0300

    Posting API updated

--- src/main/java/com/simplepost/Post.java
+++ src/main/java/com/simplepost/Post.java
@@ -1,9 +1,8 @@
 package com.simplepost;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Entity
 public class Post {
@@ -16,9 +15,15 @@ public class Post {
 
     private String text;
 
-    public Post() {}
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationTimestamp;
+
+    public Post() {
+        creationTimestamp = new Date();
+    }
 
     public Post(String title, String text) {
+        this();
         this.title = title;
         this.text = text;
     }
@@ -45,5 +50,32 @@ public class Post {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Date getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Date creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public String getCreationDateFormatted() {
+        return new SimpleDateFormat("dd.MM.yyyy").format(creationTimestamp);
+    }
+
+    public String getCreationTimeFormatted() {
+        return new SimpleDateFormat("HH:mm").format(creationTimestamp);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", creationDate=" + getCreationDateFormatted() +
+                ", creationTime=" + getCreationTimeFormatted() +
+                '}';
     }
 }
--- src/main/java/com/simplepost/PostController.java
+++ src/main/java/com/simplepost/PostController.java
@@ -5,21 +5,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/post")
+@RequestMapping(path="/posts")
 public class PostController {
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping(path="/add")
-    public @ResponseBody String addPost(@RequestParam String title, @RequestParam String text) {
+    @PostMapping("/add")
+    @ResponseBody
+    public String addPost(@RequestParam String title, @RequestParam String text) {
         Post post = new Post(title, text);
         postRepository.save(post);
-        return "Post created";
+        return "Post created:\n" + post.toString();
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<Post> getPosts() {
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String deletePost(@RequestParam Integer id) {
+        if(!postRepository.existsById(id)) return "There is no post with id " + id;
+        postRepository.deleteById(id);
+        return "Post with id " + id + " was deleted";
+    }
+
+    @DeleteMapping({"/clear", "/deleteAll"})
+    @ResponseBody
+    public String deleteAll() {
+        if(postRepository.count() == 0) return "Nothing to delete";
+        postRepository.deleteAll();
+        return "All posts were deleted";
+    }
+
+    @GetMapping({"/all", "/getAll"})
+    @ResponseBody
+    public Iterable<Post> getPosts() {
         return postRepository.findAll();
     }
 
