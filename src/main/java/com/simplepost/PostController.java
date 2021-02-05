0188b29abeff1a2a6692c9327b12e531d3c29207
GreenBudgie <forestengine@yandex.ru>
2021-02-05 21:01:29 +0300

    Basic form for adding posts

--- src/main/java/com/simplepost/PostController.java
+++ src/main/java/com/simplepost/PostController.java
@@ -2,24 +2,30 @@ package com.simplepost;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/posts")
 public class PostController {
 
     @Autowired
     private PostRepository postRepository;
 
-    @PostMapping("/add")
-    @ResponseBody
-    public String addPost(@RequestParam String title, @RequestParam String text) {
-        Post post = new Post(title, text);
-        postRepository.save(post);
-        return "Post created:\n" + post.toString();
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("post", new Post());
+        model.addAttribute("posts", getPosts());
+        return "index";
     }
 
-    @DeleteMapping("/delete")
+    @PostMapping("posts/add")
+    public String addPost(@ModelAttribute Post post, Model model) {
+        postRepository.save(post);
+        model.addAttribute("posts", getPosts());
+        return "index";
+    }
+
+    @DeleteMapping("posts/delete")
     @ResponseBody
     public String deletePost(@RequestParam Integer id) {
         if(!postRepository.existsById(id)) return "There is no post with id " + id;
@@ -27,7 +33,7 @@ public class PostController {
         return "Post with id " + id + " was deleted";
     }
 
-    @DeleteMapping({"/clear", "/deleteAll"})
+    @DeleteMapping({"posts/clear", "posts/deleteAll"})
     @ResponseBody
     public String deleteAll() {
         if(postRepository.count() == 0) return "Nothing to delete";
@@ -35,7 +41,7 @@ public class PostController {
         return "All posts were deleted";
     }
 
-    @GetMapping({"/all", "/getAll"})
+    @GetMapping({"posts/all", "posts/getAll"})
     @ResponseBody
     public Iterable<Post> getPosts() {
         return postRepository.findAll();
