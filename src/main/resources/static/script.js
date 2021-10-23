e13c05e033a865841a5ae5843efee6abd8a98802
GreenBudgie <forestengine@yandex.ru>
2021-10-23 18:47:36 +0300

    Text area appears

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,19 @@
-const editPostButton = document.querySelector(".edit-post");
-editPostButton.addEventListener("click", () => {
+let isEditingPost = false;
 
+function startPostEditing(postElement) {
+    isEditingPost = true;
+    const text = postElement.querySelector(".post-text");
+    const textarea = postElement.querySelector(".edit-post-area");
+
+    text.style = "display: none";
+    textarea.style = "display: block";
+    textarea.textContent = text.textContent;
+    textarea.focus();
+    textarea.selectionStart = textarea.textContent.length;
+}
+
+const editPostButton = document.querySelector(".edit-post");
+editPostButton.addEventListener("click", event => {
+    const postElement = event.target.parentNode.parentNode.parentNode;
+    if(!isEditingPost) startPostEditing(postElement);
 });
