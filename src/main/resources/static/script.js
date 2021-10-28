2a8cf6711b5e864cdbb3b7844029247b3c291720
GreenBudgie <forestengine@yandex.ru>
2021-10-28 14:22:27 +0300

    Fixed long posts

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,10 +5,10 @@ function startPostEditing(postElement) {
     const text = postElement.querySelector(".post-text");
     const textarea = postElement.querySelector("#edit-post-area");
     const editPostForm = postElement.querySelector("#edit-post-form");
-    console.log("start");
     text.style = "display: none";
     editPostForm.style = "display: block";
     textarea.textContent = text.textContent;
+    textarea.style.height = textarea.scrollHeight + 3 + "px";
     textarea.focus();
     textarea.selectionStart = textarea.textContent.length;
 }
